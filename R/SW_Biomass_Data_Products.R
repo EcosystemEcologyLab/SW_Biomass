166de8364219a608b5ef4a01bb04b5ae19fa3c7e
Eric Scott <scottericr@gmail.com>
2024-01-19 09:32:36 -0700

    refactor to use relative paths

--- R/SW_Biomass_Data_Products.R
+++ R/SW_Biomass_Data_Products.R
@@ -1,34 +1,29 @@
+
+# Packages ----------------------------------------------------------------
+
 library(raster)
+library(fs)
 
-# Set directory filepaths
-repo.code.fp = getwd()
-setwd('../')
-repo.data.fp = paste(getwd(), 'Data', sep = '/')
-setwd('../../')
-root.fp = getwd()
-data.fp = paste(root.fp, 'Data', 'AGB', 'Southwest', sep = '/')
-shp.fp = paste(repo.data.fp, 'Shapefiles', sep = '/')
+# Get shapefiles for spatial subsets ---------------
+sw_shps <- dir_ls("data/shapefiles/", glob = "*.shp")
+sw_box = shapefile(sw_shps[3]) #SW region
+gw_box = shapefile(sw_shps[1]) #goldwater range
+nnss_box = shapefile(sw_shps[2]) 
+ws_box = shapefile(sw_shps[4]) #white sands
 
-# Get shapefiles for spatial subsets
-sw.shps = list.files(shp.fp, pattern = '.shp', full.names = TRUE)
-sw.box = shapefile(sw.shps[3])
-gw.box = shapefile(sw.shps[1])
-nnss.box = shapefile(sw.shps[2])
-ws.box = shapefile(sw.shps[4])
-
-# ------------ Read 2010 AGB data products
+# Read 2010 AGB data products ------------ 
 
 # Chopping
-chopping.agb.2010 = stack(paste(data.fp, 'Chopping', 'MISR_agb_estimates_20002021.tif', sep = '/'))[[10]]
+chopping_agb_2010 = stack("data/rasters/Chopping/MISR_agb_estimates_20002021.tif")[[10]]
 
 # Liu
-source(paste(repo.code.fp, 'Liu_AGB_Raster_Function.R', sep = '/'))
-liu.agb = liu.raster.fun(paste(data.fp, 'Liu', sep = '/'))
-liu.agb.2010 = liu.agb[[8]]
+source('R/Liu_AGB_Raster_Function.R')
+liu_agb = liu.raster.fun("data/rasters/Liu/")
+liu_agb_2010 = liu_agb[[8]]
 
 # ESA CCI
-esa.agb.2010.f1 = raster(list.files(paste(data.fp, 'ESA_CCI', sep = '/'), pattern = '.tif', full.names = TRUE)[1])
-esa.agb.2010.f2 = raster(list.files(paste(data.fp, 'ESA_CCI', sep = '/'), pattern = '.tif', full.names = TRUE)[2])
+esa_agb_2010_f1 = raster("data/rasters/ESA_CCI/N40W110_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-fv4.0.tif")
+esa_agb_2010_f2 = raster("data/rasters/ESA_CCI/N40W120_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-fv4.0.tif") #fails
 
 # Xu
-xu.agb.2010 = stack(paste(data.fp, 'Xu', 'test10a_cd_ab_pred_corr_2000_2019_v2.tif', sep = '/'))[[10]] * 2.2
+xu_agb_2010 = stack("data/rasters/Xu/test10a_cd_ab_pred_corr_2000_2019_v2.tif")[[10]] * 2.2 #conversion from MgC/ha to Mg/ha
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SW_Biomass_Data_Products
 Exploring gridded biomass products available across the southwestern United States
+
+To reproduce:
+
+1. download the data into data/rasters/
