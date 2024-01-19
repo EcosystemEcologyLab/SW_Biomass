88099122cf794e41b932204623c3ec3fbfa15ca0
Eric Scott <scottericr@gmail.com>
2024-01-19 09:44:41 -0700

    refactor to use terra

--- R/Liu_AGB_Raster_Function.R
+++ R/Liu_AGB_Raster_Function.R
@@ -1,42 +1,49 @@
-liu.raster.fun = function(liu.fp) {
-  library(ncdf4)
+#' Read Liu .nc file and convert to raster with correct projection and units
+#'
+#' @param path the file path to the directory containing Liu .nc files
+#'
+#' @return a SpatRaster object
+#' 
+liu_nc_to_rast = function(path) {
+library(ncdf4)
   
   # Open Liu AGBc netCDF file
-  liu.nc = nc_open(list.files(liu.fp, pattern = '.nc', full.names = TRUE))
+  liu.nc <- nc_open(list.files(path, pattern = '.nc', full.names = TRUE))
   
   # Extract lat/lon attributes
-  lat = ncvar_get(liu.nc, 'latitude')
-  lon = ncvar_get(liu.nc, 'longitude')
+  lat <- ncvar_get(liu.nc, 'latitude')
+  lon <- ncvar_get(liu.nc, 'longitude')
   
   # Extract AGBc variable (matrix)
-  liu.agb.mat = ncvar_get(liu.nc, 'Aboveground Biomass Carbon')
+  liu.agb.mat <- ncvar_get(liu.nc, 'Aboveground Biomass Carbon')
   
   # Close netCDF file
   nc_close(liu.nc)
   
   # Get spatial extent of Liu dataset
-  liu.ext = c(min(lon), max(lon), min(lat), max(lat))
+  liu.ext <- c(min(lon), max(lon), min(lat), max(lat))
   
   # Set spatial coordinate reference system variable
-  liu.crs = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+  liu.crs <- '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
   
   # Create initial rasterized matrix
-  initial.ras = raster(liu.agb.mat[,,1])
-  extent(initial.ras) = liu.ext
-  crs(initial.ras) = liu.crs
+  initial.ras <- terra::rast(liu.agb.mat[,,1])
+  terra::ext(initial.ras) <- liu.ext
+  terra::crs(initial.ras) <- liu.crs
   
   # Set output raster stack for all subsequent years/matrix layers
-  out.stack = stack(initial.ras)
+  out.stack <- rast(initial.ras)
   
   for (i in 2 : dim(liu.agb.mat)[3]){
-    ras = raster(liu.agb.mat[,,i])
-    extent(ras) = liu.ext
-    out.stack = stack(ras, out.stack)
+    ras <- terra::rast(liu.agb.mat[,,i])
+    terra::ext(ras) <- liu.ext
+    terra::crs(ras) <- liu.crs
+    out.stack <- c(ras, out.stack)
   }
   
   # Set names for raster stack (years 1993 to 2012)
-  names(out.stack) = as.character(seq(1993,2012,1))
+  names(out.stack) <- as.character(seq(1993,2012,1))
   
   # Return output raster stack (multiply by 2.2 to convert from MgC/ha to Mg/ha)
-  return(out.stack * 2.2)
+  out.stack * 2.2
 }
--- R/SW_Biomass_Data_Products.R
+++ R/SW_Biomass_Data_Products.R
@@ -1,29 +1,29 @@
-
-# Packages ----------------------------------------------------------------
-
-library(raster)
+# Packages and Functions ---------------------------------------------------
+# library(raster)
+library(terra)
 library(fs)
 
+source('R/Liu_AGB_Raster_Function.R')
+
 # Get shapefiles for spatial subsets ---------------
-sw_shps <- dir_ls("data/shapefiles/", glob = "*.shp")
-sw_box = shapefile(sw_shps[3]) #SW region
-gw_box = shapefile(sw_shps[1]) #goldwater range
-nnss_box = shapefile(sw_shps[2]) 
-ws_box = shapefile(sw_shps[4]) #white sands
+sw_shps  <- dir_ls("data/shapefiles/", glob = "*.shp")
+sw_box   <- vect(sw_shps[3]) #SW region
+gw_box   <- vect(sw_shps[1]) #goldwater range
+nnss_box <- vect(sw_shps[2]) 
+ws_box   <- vect(sw_shps[4]) #white sands
 
 # Read 2010 AGB data products ------------ 
 
 # Chopping
-chopping_agb_2010 = stack("data/rasters/Chopping/MISR_agb_estimates_20002021.tif")[[10]]
+chopping_agb_2010 <- rast("data/rasters/Chopping/MISR_agb_estimates_20002021.tif")[[10]]
 
 # Liu
-source('R/Liu_AGB_Raster_Function.R')
-liu_agb = liu.raster.fun("data/rasters/Liu/")
-liu_agb_2010 = liu_agb[[8]]
+liu_agb <- liu_nc_to_rast("data/rasters/Liu/")
+liu_agb_2010 <- liu_agb[[8]]
 
 # ESA CCI
-esa_agb_2010_f1 = raster("data/rasters/ESA_CCI/N40W110_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-fv4.0.tif")
-esa_agb_2010_f2 = raster("data/rasters/ESA_CCI/N40W120_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-fv4.0.tif") #fails
+esa_agb_2010_f1 <- rast("data/rasters/ESA_CCI/N40W110_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-fv4.0.tif")
+esa_agb_2010_f2 <- rast("data/rasters/ESA_CCI/N40W120_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-fv4.0.tif") #fails to allocate memory
 
 # Xu
-xu_agb_2010 = stack("data/rasters/Xu/test10a_cd_ab_pred_corr_2000_2019_v2.tif")[[10]] * 2.2 #conversion from MgC/ha to Mg/ha
+xu_agb_2010 <- rast("data/rasters/Xu/test10a_cd_ab_pred_corr_2000_2019_v2.tif")[[10]] * 2.2 #conversion from MgC/ha to Mg/ha
