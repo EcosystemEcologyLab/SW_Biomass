fa5e28501f1a65e0c12692557f5097e346c7510c
Eric Scott <scottericr@gmail.com>
2024-01-25 11:23:54 -0700

    forgot to write chopping out

--- R/01-read_harmonize.R
+++ R/01-read_harmonize.R
@@ -16,11 +16,18 @@ ws_box   <- vect(sw_shps[4]) #white sands
 # Chopping
 chopping_agb_2010 <-
   rast("data/rasters/Chopping/MISR_agb_estimates_20002021.tif")[[10]]
+names(chopping_agb_2010) <- "chopping_agb_2010"
+varnames(chopping_agb_2010) <- "AGB"
+units(chopping_agb_2010) <- "Mg/ha"
+
 
 # Liu
 liu_agb <- liu_nc_to_rast("data/rasters/Liu/")
 liu_agb_2010 <- liu_agb[[18]]
-names(liu_agb_2010) <- varnames(liu_agb_2010) <- "AGB"
+
+units(liu_agb_2010) <- "Mg/ha"
+names(liu_agb_2010) <- "liu_agb_2010"
+varnames(liu_agb_2010) <- "AGB"
 
 # ESA CCI
 esa_agb_2010_f1 <- rast("data/rasters/ESA_CCI/N40W110_ESACCI-BIOMASS-L4-AGB-MERGED-100m-2010-fv4.0.tif", win = ext(sw_box), snap = "out")
@@ -32,7 +39,8 @@ esa_agb_2010 <- merge(esa_agb_2010_f1, esa_agb_2010_f2)
 # ext(esa_agb_2010)
 
 units(esa_agb_2010) <- "Mg/ha"
-names(esa_agb_2010) <- varnames(esa_agb_2010) <- "AGB"
+names(esa_agb_2010) <- "esa_agb_2010"
+varnames(esa_agb_2010) <- "AGB"
 
 # Xu
 xu_agb_2010 <-
@@ -41,8 +49,10 @@ xu_agb_2010 <-
     win = ext(sw_box),
     snap = "out"
   )[[10]] * 2.2 #conversion from MgC/ha to Mg/ha
+
 units(xu_agb_2010) <- "Mg/ha"
-names(xu_agb_2010) <- varnames(xu_agb_2010) <- "AGB"
+names(xu_agb_2010) <- "xu_agb_2010"
+varnames(xu_agb_2010) <- "AGB"
 
 #RAP
 #README: http://rangeland.ntsg.umt.edu/data/rap/rap-vegetation-biomass/v3/README
@@ -60,7 +70,7 @@ conv_factor <-
   units::set_units(1, "lb/acre") |> units::set_units("Mg/ha") |> as.numeric()
 rap_agb_2010 <- rap_agb_2010 * conv_factor #not sure how to speed this up.
 varnames(rap_agb_2010) <- "AGB"
-names(rap_agb_2010) <- c("AGB_annual", "AGB_perennial")
+names(rap_agb_2010) <- c("rap_agb_2010_annual", "rap_agb_2010_perennial")
 units(rap_agb_2010) <- c("Mg/ha", "Mg/ha")
 
 
@@ -114,8 +124,9 @@ sw_rap_perennial_2010 <- sw_rap_agb_2010[[2]]
 
 # Write geotiffs ----------------------------------------------------------
 
-writeRaster(sw_chopping_agb_2010, "data/rasters/harmonized/chopping.tiff")
-writeRaster(sw_liu_agb_2010, "data/rasters/harmonized/liu.tiff")
-writeRaster(sw_xu_agb_2010, "data/rasters/harmonized/xu.tiff")
-writeRaster(sw_rap_agb_2010, "data/rasters/harmonized/rap.tiff")
+writeRaster(sw_chopping_agb_2010, "data/rasters/harmonized/chopping.tiff", overwrite = TRUE)
+writeRaster(sw_liu_agb_2010, "data/rasters/harmonized/liu.tiff", overwrite = TRUE)
+writeRaster(sw_xu_agb_2010, "data/rasters/harmonized/xu.tiff", overwrite = TRUE)
+writeRaster(sw_rap_agb_2010, "data/rasters/harmonized/rap.tiff", overwrite = TRUE)
+writeRaster(sw_esa_agb_2010, "data/rasters/harmonized/esa.tiff", overwrite = TRUE)
 
