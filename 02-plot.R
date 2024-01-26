6ac1a4e00fc09fd08994d8b73cf8fbc9938aaea1
Eric Scott <scottericr@gmail.com>
2024-01-25 17:18:01 -0700

    move plotting script

--- 02-plot.R
+++ 02-plot.R
@@ -8,37 +8,28 @@ library(maps)
 
 # Read in files -----------------------------------------------------------
 
-files <- dir_ls("data/rasters/harmonized/", glob = "*.tiff")
-agb_stack <- rast(files)
+# files <- dir_ls("data/rasters/harmonized/", glob = "*.tiff")
+# agb_stack <- rast(files)
 agb_stack
 
-sw_shps  <- dir_ls("data/shapefiles/", glob = "*.shp")
-sw_box   <- vect(sw_shps[3]) #SW region
-gw_box   <- vect(sw_shps[1]) #goldwater range
-nnss_box <- vect(sw_shps[2]) 
-ws_box   <- vect(sw_shps[4]) #white sands
 
 az_border_sf <- 
-  maps::map("state", "arizona") |> 
-  st_as_sf() 
+  maps::map("state", "arizona", plot = FALSE, fill = TRUE) |> 
+  st_as_sf() |> 
+  st_transform(crs(agb_stack))
 
-az_border_vect <- 
-  az_border_sf |>
-  vect() |>
-  project(agb_stack)
-
-ext(az_border_sf)
-agb_az <- agb_stack |> crop(ext(az_border_sf))
+agb_az <- agb_stack |> crop(az_border_sf, mask = TRUE)
 
 # Plots -------------------------------------------------------------------
 
 p <- ggplot() +
-  geom_spatraster(data = agb_stack) +
-  geom_polygon(data = map_data("state", "arizona"), aes(x = long, y = lat), fill = NA, color = "black") +
-  scale_fill_viridis_c(option = "D") +
+  geom_spatraster(data = agb_az) +
+  geom_sf(data = az_border_sf, fill = NA) +
+  scale_fill_viridis_c(option = "D", na.value = "transparent") +
   coord_sf() +
   facet_wrap(~lyr)
 p
+
 ggsave("five_datasets.png", p, width = 8, height = 6)
 #why not using the full range? Chopping has some really high value pixels.
 
