77e74e6dfa02616151799b1610cc5c208ac36670
Eric Scott <scottericr@gmail.com>
2024-02-09 15:33:59 -0700

    add more notes about binwidth and gridsize

--- notes/kernel_estimation.R
+++ notes/kernel_estimation.R
@@ -1,16 +1,16 @@
 library(targets)
 library(tidyverse)
 
-tar_load(agb_stack)
+tar_load(agb_df)
 sel <- "Xu et al."
 data <- 
-  agb_stack |> 
-  as.data.frame() |> 
-  as_tibble() |> 
-  select(x = `ESA CCI`, y = `Xu et al.`)
+  agb_df |> 
+  select(x = `ESA CCI`, y = .data[[sel]])
 
 
 h <- c(bw.nrd0(data$x), bw.nrd0(data$y))
+# h <- c(KernSmooth::dpik(data$x, scalest = "stdev", gridsize = 400L),
+#        KernSmooth::dpik(data$y, scalest = "stdev", gridsize = 400L))
 h
 
 # dens <- MASS::kde2d(x = data$x, y = data$y, h = h)
@@ -18,7 +18,13 @@ h
 # MASSExtra version supposedly scales better to large data
 # dens <- MASSExtra::kde_2d(x = data$x, y = data$y, bw = h)
 
-dens <- KernSmooth::bkde2D(x = cbind(data$x, data$y), bandwidth = h, gridsize = c(200L, 200L))
+dens <-
+  KernSmooth::bkde2D(
+    x = cbind(data$x, data$y),
+    bandwidth = h,
+    gridsize = c(400L, 400L),
+    range.x = list(c(0, max(data)), c(0, max(data)))
+  )
 
 
 ix <- findInterval(data$x, dens$x1)
@@ -26,6 +32,9 @@ iy <- findInterval(data$y, dens$x2)
 ii <- cbind(ix, iy)
 # data$density <- dens$z[ii]
 data$density <- dens$fhat[ii]
+
+#TODO: shouldn't dupliacte points get summed together or somethign?  Like, isn't that the whole point?
+data |> slice_head(n = 1000) |> duplicated() |> sum()
 
 agb_max <- max(data)
 
