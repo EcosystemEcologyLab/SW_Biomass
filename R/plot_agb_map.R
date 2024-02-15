3a6b76d82b52e4953b39fc04614960787a8fc5a1
Eric Scott <scottericr@gmail.com>
2024-02-15 15:43:14 -0700

    add args for downsampling and adjust color palettes

--- R/plot_agb_map.R
+++ R/plot_agb_map.R
@@ -1,35 +1,47 @@
-#' Title
+#' Plot map of AGB faceted by data product
 #'
-#' @param agb_stack agb_stack target
-#' @param save_path file path to save plot to
-#' @param ... other arguments passed to ggsave(), e.g. height and width
-#'
-#' @return nothing, called for side effects of saving a plot
+#' @note these plots have to be created and saved in one function.  Saving the
+#'   ggplot object doesn't work with geom_spatraster
+#'   
+#' @param agb_stack SpatRaster object with layers for different data products
+#' @param downsample logical; include all pixels in the dataset or let
+#'   geom_spatraster() do it's default downsampling?
+#' @param path passed to ggsave()
+#' @param filename passed to ggsave().  Include file extension desired for output (e.g. .png or .pdf)
+#' @param ... additional arguments passed to ggsave(), e.g. `height`
+#' 
+#' @return nothing, called for side effects
 #'
 #' @examples
-#' plot_agb_map(agb_stack, width = 7, height = 6)
-plot_agb_map <- function(agb_stack, save_path = "docs/fig/agb_map.png", ...) {
-  az_border_sf <- 
-    maps::map("state", "arizona", plot = FALSE, fill = TRUE) |> 
-    st_as_sf() |> 
-    st_transform(crs(agb_stack))
+#' plot_agb_map(agb_stack)
+plot_agb_map <- function(agb_stack, downsample = TRUE, path = "docs/fig", filename = "map_agb.png", ...) {
+
+  if (isFALSE(downsample)) {
+    n <- length(values(agb_stack[[1]]))
+    p_base <- ggplot() +
+      tidyterra::geom_spatraster(data = agb_stack, maxcell = n)
+  } else {
+    p_base <- ggplot() +
+      tidyterra::geom_spatraster(data = agb_stack)
+  }
   
   #subset of colors from the scio package batlow_w palette
   map_cols <- c("#EFB298", "#C39E4B", "#7D8737", "#437153", "#185661", "#0C325D")
   
   p <-
-    ggplot() +
-    tidyterra::geom_spatraster(data = agb_stack) +
-    geom_sf(data = az_border_sf, fill = NA) +
-    # scale_fill_viridis_c(option = "D", na.value = "transparent") +
+    p_base +
     scale_fill_gradientn(colours = map_cols, na.value = "transparent") +
-    coord_sf() +
     facet_wrap(~lyr) +
     labs(fill = "AGB (Mg/ha)") +
     theme_minimal(base_size = 10) +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
-  ggsave(save_path, p, ...)
-  trim_image(save_path)
+  if (fs::path_ext(filename) %in% c("pdf", "svg", "eps", "ps")) {
+    p <- ggrastr::rasterise(p, layer = "Raster", dpi = 200, dev = "ragg_png")
+    ggsave(filename = filename, path = path, plot = p, useDingbats = FALSE, ...)
+  } else {
+    ggsave(filename = filename, path = path, plot = p, dpi = 200, ...)
+    trim_image(fs::path(path, filename))
+  }
 }
 
--- R/plot_sd_map.R
+++ R/plot_sd_map.R
@@ -1,43 +1,55 @@
-#' Title
+#' Map of standard deviation of AGB across data products
+#' 
+#' @note these plots have to be created and saved in one function.  Saving the
+#'   ggplot object doesn't work with geom_spatraster
 #'
 #' @param agb_stack agb_stack target
-#' @param save_path file path to save plot to
-#' @param ... other arguments passed to ggsave(), e.g. height and width
-#'
-#' @return nothing, called for side effects of saving a plot
+#' @param downsample logical; include all pixels in the dataset or let
+#'   geom_spatraster() do it's default downsampling?
+#' @param path passed to ggsave()
+#' @param filename passed to ggsave().  Include file extension desired for output (e.g. .png or .pdf)
+#' @param ... additional arguments passed to ggsave(), e.g. `height`
+#'   
+#' @return nothing, called for side effects
 #'
 #' @examples
-#' plot_sd_map(agb_stack, width = 7, height = 6)
-plot_sd_map <- function(agb_stack, save_path = "docs/fig/sd_map.png", ...) {
-  az_border_sf <- 
-    maps::map("state", "arizona", plot = FALSE, fill = TRUE) |> 
-    st_as_sf() |> 
-    st_transform(crs(agb_stack))
-   #TODO make this "pop".  Ideas:
-   # Use SD of 99%ile of the data instead of all the data to remove extreme values?
-   # Not sure how I'd calc quantiles---on all data products combined? 
+#' plot_sd_map(agb_stack)
+plot_sd_map <- function(agb_stack, downsample = TRUE, path = "docs/fig", filename = "map_sd.png", ...) {
   agb_sd <- agb_stack |> 
     stdev(na.rm = TRUE)
   
+  if(isFALSE(downsample)) {
+    n <- length(values(agb_sd))
+    p_base <- ggplot() +
+      tidyterra::geom_spatraster(
+        data = agb_sd,
+        maxcell = n 
+      )
+  } else {
+    p_base <- ggplot() +
+      tidyterra::geom_spatraster(data = agb_sd)
+  }
+  
   p <-
-    ggplot() +
-    tidyterra::geom_spatraster(
-      data = agb_sd,
-      maxcell = length(values(agb_sd)) # uncomment for "production"
-    ) +
-    geom_sf(data = az_border_sf, fill = NA) +
-    # TODO: make the high SD areas "pop" more
+    p_base +
     scale_fill_viridis_c(
       option = "viridis",
+      trans = "log1p", #log(x + 1) transformation
+      breaks = scales::breaks_log(6),
       na.value = "transparent",
       guide = guide_colorbar(barwidth = 0.6, title.position = "top")
     ) +
-    coord_sf() +
     theme_minimal(base_size = 9) +
     labs(fill = "SD AGB (Mg ha<sup>-1</sup>)") +
     theme(axis.text.x = element_text(angle = 45, hjust = 1),
           legend.title = element_markdown())
+  
+  if (fs::path_ext(filename) %in% c("pdf", "svg", "eps", "ps")) {
+    p <- ggrastr::rasterise(p, layer = "Raster", dpi = 200, dev = "ragg_png")
+    ggsave(filename = filename, path = path, plot = p, useDingbats = FALSE, ...)
+  } else {
+    ggsave(filename = filename, path = path, plot = p, dpi = 200, ...)
+    trim_image(fs::path(path, filename))
+  }
 
-  ggsave(save_path, p, ...)
-  trim_image(save_path)
 }
