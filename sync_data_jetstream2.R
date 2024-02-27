5e61605ff3510823e1688ba27fc39b5eb974bf27
Eric Scott <scottericr@gmail.com>
2024-02-27 15:33:23 -0700

    script to sync data files actually used to jetstream2 instance.

---
+++ sync_data_jetstream2.R
@@ -0,0 +1,27 @@
+#rsync data with jetstream2 instance mounted volume
+#recommend sourcing as background job since this may take a loooooong time
+library(fs)
+library(targets)
+library(tidyverse)
+
+file_targets <-
+  targets::tar_manifest(c(ends_with("_dir"), ends_with("_file"), ends_with("_files")))
+
+paths_local <- 
+  map(file_targets$name, \(x) {
+    do.call(tar_read, args = list(name = x))
+  }) |> list_c()
+
+#test with just shapfiles that aren't already on jestream2
+# paths_local <- paths_local[str_detect(paths_local, "shapefiles")]
+
+#write file paths to tempfile
+file_list <- fs::file_temp()
+writeLines(paths_local, file_list)
+
+#rsync rasters to jetstream mounted volume
+dir_remote <- "/media/volume/agb_rasters"
+dst <- paste("exouser@149.165.175.200", dir_remote, sep = ":")
+args <- c("-a", "-q", "-i", paste0("--files-from=", file_list), "./", dst)
+
+processx::run("rsync", args = args, echo_cmd = TRUE)
